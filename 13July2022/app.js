e96836d35a9b9b845166808dede346e5951176e4
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-13 00:56:18 +0530

    practice

--- 13July2022/app.js
+++ 13July2022/app.js
@@ -18,16 +18,16 @@ console.log(sum);
 
 
 
-
+//------------------------------------------------------------------------------------------------------------
 //Sum of diagonal element of a matrix:-
 let matrix2 = [[2,6,7,9],
               [8,1,4,5],
               [4,7,3,8],
               [1,8,6,3]]
-let row = matrix2.length;
-let column = matrix[0].length;
+let row1 = matrix2.length;
+let column = matrix2[0].length;
 let sum2 = 0;
-for(let i=0; i<row; i++){
+for(let i=0; i<row1; i++){
     for(let j=0; j<column; j++){
         if(i==j){
             sum2+=matrix[i][j];
@@ -35,3 +35,20 @@ for(let i=0; i<row; i++){
     }
 }
 console.log(sum2);
+
+
+
+
+
+
+
+let matrix3 = [[1,2,3],
+               [4,5,6],
+               [7,8,9]]
+let row2 = matrix3.length;
+let start=1;
+let sum3=0;
+for(let i=0; i<row2; i++){
+    sum3 += matrix3[i][start];
+}
+console.log(sum3)
