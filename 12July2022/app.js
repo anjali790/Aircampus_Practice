8ed9402adfbf5f3fc6097a8dab0ba11e381979a5
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-12 14:47:52 +0530

    practice

---
+++ 12July2022/app.js
@@ -0,0 +1,9 @@
+//2D Array:-
+let arrayMatrix = [[1,2,3,4],
+                   [4,5,6,7],
+                   [8,9,10,11]];
+
+//Number of rows:-
+console.log("rows of matrix",arrayMatrix.length);
+
+//Number
