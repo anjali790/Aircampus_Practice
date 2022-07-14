eae9faa7c368d6402e72930cc27a187229dfe2f5
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-14 17:39:05 +0530

    practice

---
+++ 14July2022/app.js
@@ -0,0 +1,9 @@
+//------------------------------------------------STRING----------------------------------------------------------
+//To get all the characters of a string:-
+let fname = 'anjali';
+console.log(fname[0],fname[1]);
+
+//Using loop:-
+for(let i=0; i<fname.length; i++){
+    console.log(fname[i]);
+}
