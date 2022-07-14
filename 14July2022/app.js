93d8bcb8379ef23cdcb1500957e5c43d2183ba0b
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-14 17:40:54 +0530

    practice

--- 14July2022/app.js
+++ 14July2022/app.js
@@ -7,3 +7,8 @@ console.log(fname[0],fname[1]);
 for(let i=0; i<fname.length; i++){
     console.log(fname[i]);
 }
+
+//To reverse the all chararter of a string:-
+for(let i=fname.length-1; i<=0; i--){
+    console.log(fname[i]);
+}
