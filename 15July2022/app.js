33ca4465e8eb09ff65c5ae09db0f41c65876f45f
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-15 17:25:56 +0530

    practice

--- 15July2022/app.js
+++ 15July2022/app.js
@@ -7,3 +7,23 @@ if(word1.split('').sort().join('') == word2.split('').sort().join('')){
     console.log("not a anagram");
 }
 
+
+
+
+
+//Binary search:-
+let binaryArray = [12,13,24,78,97,101];
+let start = 0;
+let num=97;
+let end = binaryArray.length;
+while(start <= end){
+    let middle = Math.floor((start+end)/2);
+    if(binaryArray[middle] == num){
+        console.log(middle);
+        break;
+    }else if(num > binaryArray[middle]){
+        start=middle+1;
+    }else{
+        end=middle-1;
+    }
+}
