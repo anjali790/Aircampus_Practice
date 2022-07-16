71cde350040586dcd663c838afa780619d1302ad
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-16 17:05:59 +0530

    practice

--- 16July2022/app.js
+++ 16July2022/app.js
@@ -1,4 +1,10 @@
-//---1---PUSH:-push()
+//---1---PUSH:-push() add element to the end of array
 let array1 = [12,45,67,89];
 array1.push(15);
-console.log(array1);
+console.log(array1); //output:- [12,45,67,89,15]
+
+
+//---1---POP:-pop() remove element from end
+let array2 = [24,45,67,32];
+array2.pop();
+console.log(array2); //output:- [24,45,67]
