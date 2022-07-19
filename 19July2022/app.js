9626cfca6bc5cf39a2b3325df4943eaf42c28802
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-19 17:51:07 +0530

    practice

---
+++ 19July2022/app.js
@@ -0,0 +1,10 @@
+//---------------------------------------------------OBJECT------------------------------------------------------
+//In JS everything is an object.
+//---1---
+let shubro = {
+    fname : 'shubro',
+    branch : 'ME',
+    place : 'new delhi',
+};
+console.log(shubro['fname'],shubro.branch);
+console.log(shubro)
