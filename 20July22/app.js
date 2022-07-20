b61dd58655f7cd39d06bb4f6868a4604b3592688
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-20 19:15:50 +0530

    practice

--- 20July22/app.js
+++ 20July22/app.js
@@ -3,3 +3,13 @@
 let array=[1,4,5,[4,8,9],[[3,9,2]]];
 console.log(array.flat());
 console.log(array.flat(2));
+
+
+
+//---2---
+let person = {
+    fname : 'Anjali',
+    branch : 'EEE',
+    location : 'New Delhi',
+};
+console.log(Object.keys(person));
