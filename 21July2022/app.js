7108d315e2adad8d0992ed0075363e7c7641d943
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-21 19:10:33 +0530

    practice

--- 21July2022/app.js
+++ 21July2022/app.js
@@ -1,8 +1,21 @@
 //add property in object:-
-let person = {
+let person1 = {
     fname : 'Anjali',
     branch : 'EEE',
     place : 'New Delhi',
 };
-person['country'] = 'India';
-console.log(person);
+person1['country'] = 'India';
+console.log(person1);
+
+
+
+
+//delete value from an object:-
+let person2 = {
+    fname : 'Anjali',
+    branch : 'EEE',
+    place : 'New Delhi',
+};
+person2['country'] = 'India';
+delete person2.branch;
+console.log(person2);
