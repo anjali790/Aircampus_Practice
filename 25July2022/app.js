6ac084f98e9c17cccb80c3fa43b067156e281aa9
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-25 19:07:48 +0530

    assignment

--- 25July2022/app.js
+++ 25July2022/app.js
@@ -7,8 +7,13 @@ let obj1 = {
 console.log(Object.keys(obj1).length);
 
 
-//---2---string to number:-
-let str='123';
-let num=parseInt(str)
-console.log(parseInt(str));
-console.log(typeof num);
+//---2---
+let obj2 = {
+    name: 'Anjali',
+    branch: 'EEE',
+    place: 'New Delhi',
+};
+console.log(Object.keys(obj2));
+
+
+
