82ac13ea4d497e84ae64dfa63579c5f61d1c1596
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-28 19:20:33 +0530

    practice

--- 28July2022/app.js
+++ 28July2022/app.js
@@ -21,3 +21,20 @@ obj.name()
 
 
 
+// //DEEP AND SHALLOW COPY
+let person={
+    name: 'anjali',
+    branch: 'EEE',
+    friend:{
+      college: 'kec',
+    }
+};
+// let copy={...person}
+let copy=JSON.stringify(person);
+copy=JSON.parse(copy);
+    
+console.log(person.name,copy.name);
+copy['name']='sakshi'
+console.log(person.name,copy.name);
+copy['friend']['college']='abs'
+console.log(person,copy);
