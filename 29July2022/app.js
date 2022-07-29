c0ef61a9a19954041f860c6f761b5d1b19b9e94f
Anjali Yadav <anjaliruby790@gmail.com>
2022-07-29 18:09:22 +0530

    practice

--- 29July2022/app.js
+++ 29July2022/app.js
@@ -13,3 +13,23 @@ function sort(arr){
     return arr;
 }
 console.log(sort([34,67,12,90,43,67]));
+
+
+
+
+
+function sort(arr){
+    for(let i=0; i<arr.length; i++){
+        for(let j=0; j<i; j++){
+            if(arr[i]>arr[j]){
+                let x=arr[i];
+                arr[i]=arr[j];
+                
+                
+                arr[j]=x;
+            }
+        }
+    }
+    return arr;
+}
+console.log(sort([34,67,12,90,43,67]));
