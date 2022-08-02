d20295412bfc07ba554ee5a6a5d68aeb7b247d79
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-02 11:25:20 +0530

    practice

--- 2Aug2022/app.js
+++ 2Aug2022/app.js
@@ -8,7 +8,35 @@ table.onmouseover = function(event) {
     target.style.background = '';
 };
 
-let ne=document.querySelector('.NE').innerHTML="DIRECTION";
-ne.addEventListener('click',()=>{
+let ne=document.querySelector('.NE');
+ne.addEventListener('mouseover',()=>{
     console.log('northEast');
-})
+});
+let e=document.querySelector('.E');
+e.addEventListener('mouseover',()=>{
+    console.log('east');
+});
+let sw=document.querySelector('.SW');
+sw.addEventListener('mouseover',()=>{
+    console.log('southWest');
+});
+let s=document.querySelector('.S');
+s.addEventListener('mouseover',()=>{
+    console.log('south');
+});
+let se=document.querySelector('.SE');
+se.addEventListener('mouseover',()=>{
+    console.log('southEast');
+});
+let w=document.querySelector('.W');
+w.addEventListener('mouseover',()=>{
+    console.log('west');
+});
+let nw=document.querySelector('.NW');
+nw.addEventListener('mouseover',()=>{
+    console.log('northWest');
+});
+let n=document.querySelector('.N');
+n.addEventListener('mouseover',()=>{
+    console.log('north');
+});
