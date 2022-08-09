5af7d4f3228170db32b0657e66b81396b92c6455
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-09 18:16:28 +0530

    practice

--- 9Aug2022/app.js
+++ 9Aug2022/app.js
@@ -2,6 +2,25 @@ const grandParent = document.querySelector('.grandParent');
 const parent = document.querySelector('.parent');
 const child = document.querySelector('.child');
 
-console.log(grandParent);
-console.log(parent);
-console.log(child);
+// console.log(grandParent);
+// console.log(parent);
+// console.log(child);
+
+grandParent.addEventListener('click',(event)=>{
+    console.log('grandParent');
+    // alert('grandParent clicked');
+},{once:true});
+
+parent.addEventListener('click',(event)=>{
+    console.log('parent');
+    // alert('parent clicked');
+});
+
+child.addEventListener('click',(event)=>{
+    console.log('child');
+    // alert('child clicked');
+});
+
+document.querySelector('body').addEventListener('click',()=>{
+    console.log('document');
+})
