2103549af1af5d9f84706fadbee7836e54cc7ca9
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-09 16:35:55 +0530

    practice

---
+++ 9Aug2022/app.js
@@ -0,0 +1,7 @@
+const grandParent = document.querySelector('.grandParent');
+const parent = document.querySelector('.parent');
+const child = document.querySelector('.child');
+
+console.log(grandParent);
+console.log(parent);
+console.log(child);
