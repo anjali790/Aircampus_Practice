a1ddd17b974a9d89f43bd752e8e0e3acf7a3c227
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-12 00:07:40 +0530

    practice

--- 11Aug2022/app.js
+++ 11Aug2022/app.js
@@ -48,6 +48,27 @@ dinnerScenario();
 
 
 
+
+
+Promise.resolve(1)
+  .then(() => console.log("promise1 resol"))
+  .catch((err) => console.log("catching 1 promise"));
+Promise.resolve(1).then(() => console.log("promise2"));
+Promise.resolve(1).then(() => console.log("promise3"));
+Promise.resolve(1).then(() => console.log("promise4"));
+Promise.resolve(1).then(() => console.log("promise1"));
+Promise.resolve(1).then(() => console.log("promise2"));
+Promise.resolve(1).then(() => console.log("promise3"));
+Promise.resolve(1).then(() => console.log("promise4"));
+Promise.resolve(1).then(() => console.log("promise1"));
+Promise.resolve(1).then(() => console.log("promise2"));
+Promise.resolve(1).then(() => console.log("promise3"));
+Promise.resolve(1).then(() => console.log("promise4"));
+
+
+
+
+
 //using promises:-
 // function preparationOfFood(){
 //     return new Promise((resolve,reject)=>{
