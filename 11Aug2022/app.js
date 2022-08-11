5de7fb53d38e32b609ee2338dbf7183e7c32b6de
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-11 19:19:12 +0530

    practice

--- 11Aug2022/app.js
+++ 11Aug2022/app.js
@@ -7,41 +7,55 @@
 //         console.log("preparing pavv bhaji");
 //     },3000);
 
-// console.log("server deliver the food to the table");
-// setTimeout(()=>{
-//     console.log("eating paav bhaji");
-// },2000);
+// // console.log("server deliver the food to the table");
+// // setTimeout(()=>{
+// //     console.log("eating paav bhaji");
+// // },2000);
 
-// console.log("paying the bill");
-// console.log("leaving the hotel");
-// }
+// // console.log("paying the bill");
+// // console.log("leaving the hotel");
+// // }
+// // dinnerScenario();
+
+
+
+// //use callbacks:-
+// function preparationOfFood(callback){
+//     setTimeout(()=>{
+//         console.log("preparing pavv bhaji");
+//         console.log("server deliver the food to the table");
+//         // eatingFood();
+//         callback();
+//     },3000);
+// };
+
+// function eatingFood(){
+//     setTimeout(()=>{
+//         console.log("eating paav bhaji");
+//         console.log("paying the bill");
+//         console.log("leaving the hotel");
+//     },2000);
+// };
+
+// function dinnerScenario(){
+//     console.log("Entering into the hotel");
+//     console.log("calling the server and order paav bajji");
+//     console.log("server information the chef to prepare food");
+// preparationOfFood(eatingFood);
+// };
 // dinnerScenario();
 
 
 
-//use callbacks:-
-function preparationOfFood(callback){
-    setTimeout(()=>{
-        console.log("preparing pavv bhaji");
-        console.log("server deliver the food to the table");
-        // eatingFood();
-        callback();
-    },3000);
-};
 
-function eatingFood(){
-    setTimeout(()=>{
-        console.log("eating paav bhaji");
-        console.log("paying the bill");
-        console.log("leaving the hotel");
-    },2000);
-};
-
-function dinnerScenario(){
-    console.log("Entering into the hotel");
-    console.log("calling the server and order paav bajji");
-    console.log("server information the chef to prepare food");
-preparationOfFood(eatingFood);
-
-};
-dinnerScenario();
+//using promises:-
+function preparationOfFood(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("preparing pavv bhaji");
+            console.log("server deliver the food to the table");
+            // eatingFood();
+            callback();
+        },3000);
+    })
+}
