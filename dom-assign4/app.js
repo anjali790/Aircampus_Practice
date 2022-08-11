044c79723a5dd6027759b10d90a885f7e9c6a161
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-12 00:46:45 +0530

    practice

---
+++ dom-assign4/app.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const todo = document.querySelector('todo');
+const todoIn = document.querySelector('.todo-in');
+const comp = document.querySelector('.comp');
+
+btn.addEventListener("click", ()=>{
+    setTimeout(()=>{
+        input.value = "";
+    })
+},0);
+if(input.value.length>0){
+    let 
+}
