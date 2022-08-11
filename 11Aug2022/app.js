83f9f2a96961cd18ef7f247086f3ed3940405cb9
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-11 18:48:33 +0530

    practice

--- 11Aug2022/app.js
+++ 11Aug2022/app.js
@@ -19,3 +19,24 @@ dinnerScenario();
 
 
 
+//use callbacks:-
+function preparationOfFood(){
+    setTimeout(()=>{
+        console.log("preparing pavv bhaji");
+        console.log("server deliver the food to the table");
+    },3000);
+};
+
+function eatingFood(){
+    setTimeout(()=>{
+        console.log("eating paav bhaji");
+    },2000);
+    console.log("paying the bill");
+    console.log("leaving the hotel");
+};
+
+function dinnerScenario(){
+    console.log("Entering into the hotel");
+    console.log("calling the server and order paav bajji");
+    console.log("server information the chef to prepare food");
+}
