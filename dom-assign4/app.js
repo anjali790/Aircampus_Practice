e81b98f5a72877bf0707abac63edf465d080cdc2
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-12 12:29:23 +0530

    practice

--- dom-assign4/app.js
+++ dom-assign4/app.js
@@ -1,14 +1,54 @@
 const input = document.querySelector('.input');
 const btn = document.querySelector('.btn');
-const todo = document.querySelector('todo');
+const todo = document.querySelector('.todo');
 const todoIn = document.querySelector('.todo-in');
-const comp = document.querySelector('.comp');
+const comp = document.querySelector('.complete');
 
-btn.addEventListener("click", ()=>{
-    setTimeout(()=>{
+btn.addEventListener("click", function(){
+    setTimeout(function(){
         input.value = "";
-    })
-},0);
-if(input.value.length>0){
-    let 
-}
+    }, 1);
+
+    if (input.value.length > 0) {
+        let todoOuterDiv = document.createElement('div');
+        let check = document.createElement("input");
+        check.type = "checkbox";
+        let todoInput = document.createElement("input");
+        let button1 = document.createElement("button");
+        button1.innerText = "Edit";
+        let button2 = document.createElement("button");
+        button2.innerText = "Delete";
+
+        todoOuterDiv.append(check, todoInput, button1, button2);
+        todoIn.append(todoOuterDiv);
+
+        todoInput.value = input.value;
+        todoInput.setAttribute("readonly", "readonly");
+
+
+        button1.addEventListener('click', function(){
+            if (button1.innerText == "Edit") {
+                button1.innerText = "Save";
+                todoInput.removeAttribute('readonly');
+            } else {
+                button1.innerText = "Edit";
+                todoInput.setAttribute('readonly', 'readonly');
+            }
+        });
+
+        button2.addEventListener('click', function() {
+            button2.parentElement.remove();
+        });
+
+        let compDiv = document.createElement("div");
+        comp.append(compDiv);
+
+        check.addEventListener('click', function(){
+            check.parentElement.remove();
+            compDiv.append(todoOuterDiv);
+            button1.remove();
+            todoInput.style.border = "none";
+            todoInput.style.textDecoration = "line-through";
+        })
+    }
+});
