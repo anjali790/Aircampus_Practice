369d3ff6609381bc2a96861f2bda2dccec35166a
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-20 19:43:00 +0530

    practice

--- 20Aug2022/app.js
+++ 20Aug2022/app.js
@@ -6,13 +6,26 @@ function fname(){
  fname();
 //  console.log(name);//throw an error becoz var is function scope
 
+
+
+
+function count(){
+    var countNum = 1;
+    countNum++;
+    console.log(countNum);
+}
+count();//2
+count();//2
+count();//2
+
+
+
 var count = 1;
 function counter(){
-    // var count = 1;
     count++;
     console.log(count);
 }
-counter();
-counter();
-counter();
+counter();//2
+counter();//3
+counter();//4
 
