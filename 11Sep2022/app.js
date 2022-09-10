4fed78e5629c5e61b40e6add638eaf62b057ba9d
anjali790 <anjaliruby790@gmail.com>
2022-09-11 02:33:02 +0530

    practice

---
+++ 11Sep2022/app.js
@@ -0,0 +1,12 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+function twoSum(nums,target){
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length;j++){
+            if(nums[j]+nums[i] == target){
+            return [i,j];
+            }
+        }
+    }
+
+}
+console.log(twoSum([3,3],6));
